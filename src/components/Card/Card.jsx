7829e1ea7f8ca5745e01bbf331ae3ad84cf8756f
Franco Balich <franco.balich@gmail.com>
2024-03-15 23:48:40 -0300

    Change: Move function to cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react'
 import './Card.css'
 
-export const Card = ({title="Title",text="-",animation}) => {
+export const Card = ({title="Title",text="-"}) => {
+  const [animation, setAnimation] = useState('')
+  const handlePrevius = () => {
+    setAnimation('cardToLeft')
+    setTimeout(() => {setAnimation('hidden') }, 1000)
+  }
+  const handleOmit = () => {
+    setAnimation('cardToBottom')
+    setTimeout(() => {setAnimation('hidden') }, 1000)
+  }
+  const handleNext = () => {
+    setAnimation('cardToRight')
+    setTimeout(() => {setAnimation('hidden') }, 1000)
+  }
   return (
     <div className={`card ${animation}`}>
       <h3>{title}</h3>
       <p>{text}</p>
+      <div>
+        <button onClick={handlePrevius}>Anterior</button>
+        <button onClick={handleOmit}>Omitir</button>
+        <button onClick={handleNext}>Siguiente</button>
+      </div>
     </div>
   )
 }
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -4,31 +4,26 @@ import './Game.css'
 import questionData from '../../data/questions.json'
 
 export const Game = () => {
-  const [animation, setAnimation] = useState('')
-  const [questionValue, setQuestionValue] = useState([{}])
-  const handlePrevius = () => {
-    setAnimation('cardToLeft')
-    setTimeout(() => { }, 1000)
-  }
-  const handleOmit = () => {
 
-  }
-  const handleNext = () => {
-    setAnimation('cardToRight')
+  const [questions, setQuestions] = useState(<></>)
+
+  const makeCards = () => {
+    let questionList = []
+    let i = 0
+    questionData.forEach(question => {
+      questionList.push(<Card key={i} title={question.theme} text={question.question} />)
+      i++
+    });
+    return questionList
   }
   useEffect(() => {
-    setQuestionValue(questionData[0])
+    setQuestions(makeCards())
+    console.log(questions.lenght);
   }, [])
-  useEffect(() => {
-  }, [questionValue])
+
   return (
     <section className='gameContainer'>
-      <Card animation={animation} title={questionValue.theme} text={questionValue.question} />
-      <div>
-        <button onClick={handlePrevius}>Anterior</button>
-        <button onClick={handleOmit}>Omitir</button>
-        <button onClick={handleNext}>Siguiente</button>
-      </div>
+      {questions}
     </section>
   )
 }
