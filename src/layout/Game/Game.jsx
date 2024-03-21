674435d258cb335725bc888513c6047ba6930473
Franco Balich <franco.balich@gmail.com>
2024-03-21 13:23:54 -0300

    Feat: Use Cards component

--- src/layout/Game/Game.jsx
+++ src/layout/Game/Game.jsx
@@ -1,40 +1,11 @@
-import { useEffect, useState } from 'react'
-import { Card } from '../../components/Card/Card'
 import './Game.css'
-import questionData from '../../data/questions.json'
+import { Cards } from '../../components/Cards/Cards'
 
 export const Game = () => {
 
-  const [questions, setQuestions] = useState(<></>)
-
-  const removeQuestion = (id) => {
-    let questionsAux = [];
-    questionsAux = questions;
-    console.log(questions);
-    let newList=questionsAux.filter(x=>x.id!==id)
-    console.log(newList);
-  }
-  const makeCards = () => {
-    let questionList = []
-    questionData.forEach(question => {
-      questionList.push(<Card
-        key={question.id}
-        id={question.id}
-        title={question.theme}
-        text={question.question}
-        clb={removeQuestion}
-      />)
-    });
-    return questionList
-  }
-
-  useEffect(() => {
-    setQuestions(makeCards())
-  }, [])
-
   return (
     <section className='gameContainer'>
-      {questions}
+      <Cards />
     </section>
   )
 }
