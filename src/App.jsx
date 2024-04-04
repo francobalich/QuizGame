8b2f1f23d8f8b8800378eed08133108fe53893ff
Franco Balich <franco.balich@gmail.com>
2024-04-03 21:46:13 -0300

    Feat: Add new route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Game } from './layout/Game/Game'
 import { Home } from './layout/Home/Home'
+import { Test } from './components/Test'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/game' element={<Game />} />
+      <Route path='/info' element={<Test />} />
       <Route path='/*' element={<Navigate to='/' />} />
     </Routes>
   )
